c4340e35bd9d71e3d87d0643cb61cb25695b703a
John Spong <jspong@gmail.com>
2018-10-18 23:10:31 -0700

    Initial Scene
    Robot boy runs + jumps, and the camera spins and follows

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject playerToFollow;
+
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 lerped = Vector3.Lerp(cam.transform.position, playerToFollow.transform.position, 0.5f);
+        cam.transform.position = new Vector3(lerped.x, lerped.y, cam.transform.position.z);
+        cam.transform.Rotate(cam.transform.position, 1f);
+    }
+}
